14729e8e04eee748637175086169802e016618c0
oopskim <oopskim@172.20.10.2>
2018-03-10 11:40:37 +0900

    PHPに変化を与えているGitHubに変換が起きるかを確認する

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>here is japan</title>
     </head>
     <body>
         <?php
-        // put your code here
+        echo "ここは日本です";
         ?>
     </body>
 </html>
